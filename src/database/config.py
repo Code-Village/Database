b2b09699a08ca4820b2e63db2612a48322ae683c
PromotezCitizen <gkstkdgus821@gmail.com>
2022-08-10 16:41:28 +0900

    db 경로 재설정

--- src/database/config.py
+++ src/database/config.py
@@ -1,9 +1,9 @@
 db = {
-    'user': 'root',
-    'password': 'mysql',
-    'host': 'localhost',
+    'user': 'cmckxe4j83zhap35',
+    'password': 'zaqwef7wzmt0ciir',
+    'host': 'cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     'port': 3306,
-    'database': 'test'
+    'database': 'crxhrzme3der287f'
 }
 
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@" \
