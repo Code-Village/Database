3346c69827cfc7ccc4bc5523243ba3fe75d02dfb
PromotezCitizen <gkstkdgus821@gmail.com>
2022-08-10 16:21:44 +0900

    Procfile 수정

--- src/database/config.py
+++ src/database/config.py
@@ -1,7 +1,7 @@
 db = {
     'user': 'root',
     'password': 'mysql',
-    'host': '0.0.0.0',
+    'host': 'localhost',
     'port': 3306,
     'database': 'test'
 }
--- src/database/main.py
+++ src/database/main.py
@@ -6,37 +6,34 @@ from sqlalchemy import create_engine, text
 load_dotenv()
 
 
-def createApp():
-    app = Flask(__name__)
-    app.config.from_pyfile("config.py")
+app = Flask(__name__)
+app.config.from_pyfile("config.py")
 
-    print(app.config)
+print(app.config)
 
-    database = create_engine(app.config['DB_URL'], encoding='utf8', max_overflow=0)
-    app.database = database
+database = create_engine(app.config['DB_URL'], encoding='utf8', max_overflow=0)
+app.database = database
 
-    @app.route("/")
-    def main():
-        return {"hello": "world!"}
+@app.route("/")
+def main():
+    return {"hello": "world!"}
 
 
-    @app.route("/user")
-    def user():
-        return {"name": "main"}
+@app.route("/user")
+def user():
+    return {"name": "main"}
 
-    @app.route("/db")
-    def db():
-        row = app.database.execute("""
-            select *
-            from users
-        """).fetchall()
+@app.route("/db")
+def db():
+    row = app.database.execute("""
+        select *
+        from users
+    """).fetchall()
 
-        return jsonify([ {'id': data['id'], 'pw': data['pw']} for data in row ])
-
-    return app
+    return jsonify([ {'id': data['id'], 'pw': data['pw']} for data in row ])
 
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
-    createApp().run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
