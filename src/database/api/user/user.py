42b11266bafbe59fe0bd3d821eeb0e1bf8a0fad9
PromotezCitezen <gkstkdgus821@gmail.com>
2022-08-11 23:44:34 +0900

    [register] 회원가입 query 작성 - 가입 전 데이터 중복 검사, 가입

--- src/database/api/user/user.py
+++ src/database/api/user/user.py
@@ -19,35 +19,75 @@ user_fields = User.model('User', {  # Model 객체 생성
     'data': fields.String(description='key', required=True, example="님이 적은 것")
 })
 
-@User.route('/get-all-data')
-class UserTest(Resource):
+get_user_data_parser = reqparse.RequestParser() # 대부분 id, 닉네임 검색
+get_user_data_parser.add_argument('col', type=str, required=True, help='데이터베이스에서 검색할 값이 있는 열')
+get_user_data_parser.add_argument('data', type=str, required=True, help='데이터베이스에서 검색할 값')
+
+post_user_data_parser = reqparse.RequestParser()
+post_user_data_parser.add_argument('id', type=str, required=True, help='가입할 유저의 id')
+post_user_data_parser.add_argument('pw', type=str, required=True, help='가입할 유저의 pw')
+post_user_data_parser.add_argument('nickname', type=str, required=True, help='가입할 유저의 닉네임')
+
+@User.route('/regist') # 회원가입시 이용
+class UserRegist(Resource):
+    @User.doc(
+    parser=get_user_data_parser,
+    responses={
+        200: 'Can regist',
+        201: 'Duplicate Data in Database',
+    })
     def get(self):
-        """모든 user의 데이터 받아옴"""
+        """params 필요. 해당 params가 있는 것만 검색 -> 중복 방지용. id,nickname 중복 검사에 사용"""
+        args = request.args
+        
+        query = f"SELECT COUNT(IF({args['col']}='{args['data']}',1,NULL)) as cnt FROM users"
+        rows = [ list(row) for row in database.execute(query).fetchall()]
 
-        row = database.execute("""
-            select *
-            from users
-        """).fetchall()
+        if rows[0][0] > 0:
+            return 201
+        else:
+            return 200
 
-        return jsonify([ {'id': data['id'], 'pw': data['pw']} for data in row ])
+    @User.doc(
+    parser=post_user_data_parser,
+    responses={
+        200: 'Can regist',
+        201: 'Duplicate Data in Database',
+    })
+    def post(self):
+        """id, pw, nickname을 데이터베이스에 전달"""
+        args = request.args
+
+        id = args['id']
+        pw = args['pw']
+        nickname = args['nickname']
+
+        query = f"INSERT INTO users (id, pw, nickname) VALUES ('{id}', '{pw}', '{nickname}');"
+        database.execute(query)
+
+        return 200
 
 
 get_parser = reqparse.RequestParser()
-get_parser.add_argument('col', type=str, required=True ,help='데이터베이스에서 검색할 값이 있는 열')
-get_parser.add_argument('data', type=str, required=True ,help='데이터베이스에서 검색할 값')
+get_parser.add_argument('col', type=str, required=True, help='데이터베이스에서 검색할 값이 있는 열')
+get_parser.add_argument('data', type=str, required=True, help='데이터베이스에서 검색할 값')
 
 put_parser = reqparse.RequestParser()
-put_parser.add_argument('id', type=str, required=True ,help="데이터베이스에서 변경하고자 하는 유저의, 아이디")
-put_parser.add_argument('col', type=str, required=True ,help='데이터베이스에서 변경할 값이 있는 열')
-put_parser.add_argument('data', type=str, required=True ,help='데이터베이스에서 변경할 값')
+put_parser.add_argument('id', type=str, required=True, help="데이터베이스에서 변경하고자 하는 유저의, 아이디")
+put_parser.add_argument('col', type=str, required=True, help='데이터베이스에서 변경할 값이 있는 열')
+put_parser.add_argument('data', type=str, required=True, help='데이터베이스에서 변경할 값')
 
-@User.route('/')
+del_parser = reqparse.RequestParser()
+del_parser.add_argument('id', type=str, required=True, help='데이터베이스에서 삭제할 아이디')
+
+@User.route('')
 class UserData(Resource):
     @User.doc(
         parser=get_parser,
         responses={
             200: 'Success',
-            400: 'Not in database'
+            400: 'Fatal error',
+            401: 'Not in database'
         })
     def get(self):
         """column에 해당 값이 있을 경우 가져옴"""
@@ -62,20 +102,21 @@ class UserData(Resource):
                 where {col}={arg}
             """).fetchone()
         except:
-            return {'search-col': f'{col}', 'search-data': 'None'}, 400
+            return 400
 
-        try:
-            return {'search-col': f'{col}', 'search-data': dict([ (key, row[key]) for key in row.keys()])}, 200
+        try: 
+            row.keys()
         except:
-            return {'search-col': f'{col}', 'search-data': 'None'}, 400
+            return 401
+
+        return {'search-col': f'{col}', 'search-data': dict([ (key, row[key]) for key in row.keys()])}, 200
 
 
     @User.doc(
         parser=put_parser,
         responses={
-            200: 'Success',
-            400: 'Not in database',
-            401: 'Cant change value'
+            200: 'Query succeed',
+            400: 'Fatal error'
         })
     def put(self):
         """id를 제외한 값들 업데이트 가능"""
@@ -88,12 +129,35 @@ class UserData(Resource):
         user_id = request.args['id']
         arg = request.args['data']
         
+        try:
+            database.execute(f"""
+                UPDATE users
+                SET {col}='{arg}'
+                WHERE id='{user_id}'
+            """)
+        except:
+            return 400
 
-        database.execute(f"""
-            UPDATE users
-            SET {col}='{arg}'
-            WHERE id='{user_id}'
-        """)
-        return {'data':'asdf'}
-        
-        
+        return 200
+
+    @User.doc(
+        parser=del_parser,
+        responses={
+            200: 'Query succeed',
+            400: 'Fatal error'
+        })
+    def delete(self):
+        """해당 id를 가진 계정 삭제"""
+        args = request.args
+
+        user_id = request.args['id']
+
+        try:
+            database.execute(f"""
+                DELETE
+                FROM users
+                WHERE id='{user_id}'
+            """)
+            return 401
+        except:
+            return 400
