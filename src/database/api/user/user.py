3aeafad507b6e89dc28d7ec27ef6efa1be2feca9
PromotezCitezen <gkstkdgus821@gmail.com>
2022-08-11 22:28:11 +0900

    유저 데이터 변경 기능 구현 완료

--- src/database/api/user/user.py
+++ src/database/api/user/user.py
@@ -1,6 +1,8 @@
-from flask import jsonify
-from flask_restx import Resource, Namespace, fields
+from importlib.metadata import requires
+from flask import jsonify, request
+from flask_restx import Resource, Namespace, fields, reqparse
 from sqlalchemy import create_engine
+import json
 
 from src.database.config import *
 
@@ -10,16 +12,18 @@ database = create_engine(DB_URL, encoding='utf8', max_overflow=0)
 
 User = Namespace(
     name="User",
-    description="테스트를 위해 작성한 API.",
+    description="from database get user data",
 )
 
 user_fields = User.model('User', {  # Model 객체 생성
     'data': fields.String(description='key', required=True, example="님이 적은 것")
 })
 
-@User.route('')
+@User.route('/get-all-data')
 class UserTest(Resource):
     def get(self):
+        """모든 user의 데이터 받아옴"""
+
         row = database.execute("""
             select *
             from users
@@ -27,12 +31,69 @@ class UserTest(Resource):
 
         return jsonify([ {'id': data['id'], 'pw': data['pw']} for data in row ])
 
-@User.route('/<string:id>')
-class UserData(Resource, id):
+
+get_parser = reqparse.RequestParser()
+get_parser.add_argument('col', type=str, required=True ,help='데이터베이스에서 검색할 값이 있는 열')
+get_parser.add_argument('data', type=str, required=True ,help='데이터베이스에서 검색할 값')
+
+put_parser = reqparse.RequestParser()
+put_parser.add_argument('id', type=str, required=True ,help="데이터베이스에서 변경하고자 하는 유저의, 아이디")
+put_parser.add_argument('col', type=str, required=True ,help='데이터베이스에서 변경할 값이 있는 열')
+put_parser.add_argument('data', type=str, required=True ,help='데이터베이스에서 변경할 값')
+
+@User.route('/')
+class UserData(Resource):
+    @User.doc(
+        parser=get_parser,
+        responses={
+            200: 'Success',
+            400: 'Not in database'
+        })
     def get(self):
-        row = database.execute(f"""
-            select {id}
-            from users
-        """).fetchone()
+        """column에 해당 값이 있을 경우 가져옴"""
+        col = request.args['col']
+        arg = f'{request.args["data"]}'
+        arg = f'"{arg}"'
         
-        return jsonify([ {'id': data['id'], 'pw': data['pw']} for data in row ])
+        try:
+            row = database.execute(f"""
+                select *
+                from users 
+                where {col}={arg}
+            """).fetchone()
+        except:
+            return {'search-col': f'{col}', 'search-data': 'None'}, 400
+
+        try:
+            return {'search-col': f'{col}', 'search-data': dict([ (key, row[key]) for key in row.keys()])}, 200
+        except:
+            return {'search-col': f'{col}', 'search-data': 'None'}, 400
+
+
+    @User.doc(
+        parser=put_parser,
+        responses={
+            200: 'Success',
+            400: 'Not in database',
+            401: 'Cant change value'
+        })
+    def put(self):
+        """id를 제외한 값들 업데이트 가능"""
+        args = request.args
+        col = args['col']
+
+        if col=="id":
+            return 401
+
+        user_id = request.args['id']
+        arg = request.args['data']
+        
+
+        database.execute(f"""
+            UPDATE users
+            SET {col}='{arg}'
+            WHERE id='{user_id}'
+        """)
+        return {'data':'asdf'}
+        
+        
--- src/database/main.py
+++ src/database/main.py
@@ -1,27 +1,28 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-# from flask_restx import Api, Resource, reqparse
+from flask_restx import Api, Resource, reqparse
 
 from src.database.api.user import *
 
 load_dotenv()
 
 app = Flask(__name__)
-# app.config.SWAGGER_UI_DOC_EXPANSION = 'full'
+app.config.SWAGGER_UI_DOC_EXPANSION = 'full'
 
-# api = Api(
-#     app,
-#     version=0.1, 
-#     title="Code Village", 
-#     description="API for {Code Village}", 
-#     terms_url="/",
-#     contact="gkstkdgus821@gmail.com", 
-#     license='BSD 3-Clause "New" or "Revised" License'
-# )
 
-# api.add_namespace(User, '/user')
+api = Api(
+    app,
+    version=0.1, 
+    title="Code Village - DB", 
+    description="API for {Code Village}", 
+    terms_url="/",
+    contact="gkstkdgus821@gmail.com", 
+    license='BSD 3-Clause "New" or "Revised" License'
+)
+
+api.add_namespace(User, '/user')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=5000, debug=True)
